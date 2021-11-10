25a78f2c5458c17910ff0cc4dbd6a56a01c0c686
OfekGevaCoder <94046915+OfekGevaCoder@users.noreply.github.com>
2021-11-10 13:26:03 +0200

    Update Board.h

--- Board.h
+++ Board.h
@@ -8,7 +8,7 @@ using namespace std;
 #define O ' '
 
 class Board {
-	enum {HEIGHT=25 , WIDTH=80};
+	enum {HEIGHT=23 , WIDTH=80};
 
 	char board[HEIGHT][WIDTH]
 	{ H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,O,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,
@@ -21,9 +21,7 @@ class Board {
 	 H,S,H,H,H,S,H,H,S,H,H,H,H,H,H,H,S,H,H,H,H,S,H,S,S,S,S,H,S,S,H,S,S,H,H,H,S,S,S,H,S,S,S,S,S,H,S,H,S,S,H,S,H,S,S,S,S,H,S,H,H,H,H,S,H,H,H,H,H,H,H,S,H,H,S,H,H,H,S,H,
 	 H,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,H,H,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,H,H,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,H,
 	 H,S,H,H,H,H,H,H,H,H,H,H,H,H,H,H,S,H,H,S,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,S,S,S,S,S,S,S,S,S,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,S,H,H,S,H,H,H,H,H,H,H,H,H,H,H,H,H,H,S,H,
-	 H,S,H,H,H,H,H,H,H,H,H,H,H,H,H,H,S,H,H,S,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,S,H,H,S,S,S,H,H,S,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,S,H,H,S,H,H,H,H,H,H,H,H,H,H,H,H,H,H,S,H,
 	 O,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,H,H,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,H,H,S,S,S,H,H,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,H,H,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,O,
-	 H,S,H,H,H,H,H,H,H,H,S,H,H,H,H,H,S,H,H,S,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,S,H,H,H,H,H,H,H,S,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,S,H,H,S,H,H,H,H,S,H,H,H,H,H,H,H,H,H,S,H,
 	 H,S,H,H,H,H,H,H,H,H,S,H,H,H,H,H,S,H,H,S,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,S,H,H,H,H,H,H,H,S,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,H,S,H,H,S,H,H,H,H,S,H,H,H,H,H,H,H,H,H,S,H,
 	 H,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,H,H,H,H,H,H,S,S,S,S,S,S,S,S,S,H,H,H,H,H,H,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,S,H,
 	 H,S,H,H,H,H,H,H,H,H,S,H,H,H,H,H,H,H,H,H,H,S,H,H,S,H,H,H,S,H,H,H,H,H,H,S,H,H,H,S,H,H,H,S,H,H,H,H,H,H,S,H,H,H,H,S,H,H,S,H,H,H,H,H,H,H,S,S,S,H,H,H,H,H,H,H,H,H,S,H,
